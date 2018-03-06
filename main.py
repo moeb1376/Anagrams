37b76985c42ffb5023fe85e8eb2618014a04e9b9
moeb <ebimosi14@gmail.com>
2018-03-07 02:09:21 +0330

    add dict base anagrams

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import time
+
+
 def set_words():
     words = set()
     with open('words.txt', 'r') as f:
@@ -7,13 +10,46 @@ def set_words():
     return words
 
 
-def get_yechizi(word):
+def get_permutation(word):
     result = []
     if len(word) == 1:
         return word
     for i in range(len(word)):
-        temp = get_yechizi(word[:i] + word[i + 1:])
+        temp = get_permutation(word[:i] + word[i + 1:])
         result.extend([word[i] + j for j in temp])
     return result
+
+
+t = time.time()
 words = set_words()
-print(words.intersection(set(get_yechizi('مرگ'))))
+print(words.intersection(set(get_permutation('مرگ'))))
+print(time.time() - t)
+del words
+
+
+def set_words2():
+    words2 = {}
+    with open('words.txt', 'r') as f:
+        hamze = ord('ء')
+        for line in f:
+            word = line.strip()
+            count = 0
+            for c in word:
+                count += (ord(c) - hamze)
+            data = words2.get(count, [])
+            if data != []:
+                words2[count].append(word)
+            else:
+                words2[count] = [word]
+    return words2
+
+t = time.time()
+words = set_words2()
+hamze = ord('ء')
+count = sum([ord(i)-hamze for i in 'مرگ'])
+search_area = words[count]
+per = get_permutation('مرگ')
+for i in per:
+    if i in search_area:
+        print(i)
+print(time.time() - t)
